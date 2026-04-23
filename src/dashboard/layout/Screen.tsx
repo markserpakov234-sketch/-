068b50bce025b92ddda86e42dcdddc81ddec908e
markserpakov234-sketch <markserpakov234@gmail.com>
2026-04-23 11:07:13 +0300

    Город (#4)
    * поменяли майн
    
    * новинка
    
    * чиним
    
    * длинная реклама

--- src/dashboard/layout/Screen.tsx
+++ src/dashboard/layout/Screen.tsx
@@ -2,11 +2,11 @@ import Header from '../blocks/Header';
 import Ads from '../blocks/Ads';
 import Rating from '../blocks/Rating';
 import Schedule from '../blocks/Schedule';
-import Event from '../blocks/Event';
 
 export default function Screen() {
   return (
     <div className="h-screen w-full grid grid-cols-3 gap-3 p-3 bg-black text-white">
+      
       {/* LEFT: RATING */}
       <div className="bg-white/5 rounded-2xl p-4 overflow-hidden">
         <Rating />
@@ -23,18 +23,11 @@ export default function Screen() {
         </div>
       </div>
 
-      {/* RIGHT: ADS + EVENT (ВАЖНО — FLEX ВЕРТИКАЛЬНО) */}
-      <div className="flex flex-col gap-3">
-        {/* ADS = 3/4 */}
-        <div className="flex-[3] bg-orange-500/10 rounded-2xl p-4 overflow-hidden">
-          <Ads />
-        </div>
-
-        {/* EVENT = 1/4 */}
-        <div className="flex-[1] bg-yellow-500/10 rounded-2xl p-4 overflow-hidden">
-          <Event />
-        </div>
+      {/* RIGHT: ADS FULL HEIGHT */}
+      <div className="bg-orange-500/10 rounded-2xl p-4 overflow-hidden">
+        <Ads />
       </div>
+
     </div>
   );
 }
